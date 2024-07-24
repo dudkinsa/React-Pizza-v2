8e059c80f9f8e25f27cf5d9d12abc6a66e650f7a
Dudkin Sergey <dsergik@mail.ru>
2024-07-24 17:48:18 +0300

    использование useLocation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -190,6 +190,7 @@ const Home = () => {
 
             }
             <Pagination currentPage={currentPage} onChangePage={onChangePage} />
+            
         </div>
 
     )
--- src/redux/slices/pizzaSlice.js
+++ src/redux/slices/pizzaSlice.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params) => {
+export const fetchPizzas = createAsyncThunk('pizza/fetchPizzasStatus', async (params, thunkApi) => {
     const { sortBy, order, category, search, currentPage } = params;
-    const { data } = await axios.get(`https://-66865ecb83c983911b01f11a.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`);
+    const { data } = await axios.get(`https://66865ecb83c983911b01f11a.mockapi.io/items?page=${currentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}${search}`);
 
     return data;
 });
